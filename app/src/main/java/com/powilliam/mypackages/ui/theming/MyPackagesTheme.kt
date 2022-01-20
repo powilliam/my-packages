ecb54b1803bb7217d42c67b8a62470d94bf88459
William Porto <william.porto@nave.rs>
2022-01-19 22:05:56 -0300

    feature(ui): supporting insets and system ui controller

--- app/src/main/java/com/powilliam/mypackages/ui/theming/MyPackagesTheme.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/theming/MyPackagesTheme.kt
@@ -6,6 +6,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.powilliam.mypackages.constants.SystemFeatures
 
 @Composable
@@ -15,11 +18,19 @@ fun MyPackagesTheme(
     isDarkModeEnabled: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
     val colorScheme = when {
         isDynamicColorSchemeSupported && isDarkModeEnabled -> dynamicDarkColorScheme(context())
         isDynamicColorSchemeSupported && !isDarkModeEnabled -> dynamicLightColorScheme(context())
         !isDynamicColorSchemeSupported && isDarkModeEnabled -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = !isDarkModeEnabled
+        )
     }
 
     MaterialTheme(colorScheme, NotoSansTypography, content)
