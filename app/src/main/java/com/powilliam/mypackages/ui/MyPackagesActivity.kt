3c69f230c9d0d513c46fdc424a48b5999acfb618
William Porto <william.porto@nave.rs>
2022-01-21 12:53:20 -0300

    feature(ui): adding one tap sign in

--- app/src/main/java/com/powilliam/mypackages/ui/MyPackagesActivity.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/MyPackagesActivity.kt
@@ -3,15 +3,25 @@ package com.powilliam.mypackages.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.powilliam.mypackages.di.OneTapSignInRequest
 import com.powilliam.mypackages.ui.theming.MyPackagesTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MyPackagesActivity : ComponentActivity() {
+    @OneTapSignInRequest
+    @Inject
+    lateinit var oneTapSignInRequest: BeginSignInRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -25,8 +35,13 @@ class MyPackagesActivity : ComponentActivity() {
     private fun Content() {
         ProvideWindowInsets {
             MyPackagesTheme({ this }) {
-                NavigationGraph()
+                NavigationGraph { beginSignIn() }
             }
         }
+    }
+
+    private suspend fun beginSignIn(): IntentSenderRequest {
+        val signIn = Identity.getSignInClient(this).beginSignIn(oneTapSignInRequest).await()
+        return IntentSenderRequest.Builder(signIn.pendingIntent.intentSender).build()
     }
 }
