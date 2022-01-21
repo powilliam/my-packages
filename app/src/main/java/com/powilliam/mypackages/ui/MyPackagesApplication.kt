6459ca86ced1618def9bd7f000d77b522718acb9
William Porto <william.porto@nave.rs>
2022-01-20 23:22:41 -0300

    feature(firebase-config): initialize remote config on Application::onCreate

--- app/src/main/java/com/powilliam/mypackages/ui/MyPackagesApplication.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/MyPackagesApplication.kt
@@ -1,7 +1,38 @@
 package com.powilliam.mypackages.ui
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyPackagesApplication : Application()
+class MyPackagesApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        onInitializeFirebaseConfig()
+    }
+
+    private fun onInitializeFirebaseConfig() {
+        val remoteConfig = Firebase.remoteConfig
+        val settings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+
+        remoteConfig.setConfigSettingsAsync(settings)
+        remoteConfig.fetchAndActivate()
+            .addOnSuccessListener {
+                Log.i(
+                    "fetchAndActivate",
+                    "Successfully fetched and activated"
+                )
+            }
+            .addOnFailureListener { throwable ->
+                Log.e(
+                    "fetchAndActivate",
+                    throwable.message ?: "Failed to fetch and activate remote config"
+                )
+            }
+    }
+}
