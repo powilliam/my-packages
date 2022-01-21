fcc50cf692be1afb771fd48679607a2da02e38ba
William Porto <william.porto@nave.rs>
2022-01-21 12:53:20 -0300

    feature(ui): adding one tap sign in

--- app/src/main/java/com/powilliam/mypackages/ui/NavigationGraph.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/NavigationGraph.kt
@@ -1,27 +1,60 @@
 package com.powilliam.mypackages.ui
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.auth.api.identity.Identity
 import com.powilliam.mypackages.ui.screens.PackageScreen
 import com.powilliam.mypackages.ui.screens.AddPackageScreen
 import com.powilliam.mypackages.ui.screens.EditPackageScreen
 import com.powilliam.mypackages.ui.screens.PackagesMapScreen
 import com.powilliam.mypackages.ui.screens.SearchPackageScreen
+import com.powilliam.mypackages.ui.viewmodels.PackagesMapUiState
+import com.powilliam.mypackages.ui.viewmodels.PackagesMapViewModel
 
 @Composable
-fun NavigationGraph() {
+fun NavigationGraph(beginSignIn: suspend () -> IntentSenderRequest) {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = Destination.PackagesMap.route) {
-        addPackagesScreen()
+        addPackagesScreen { beginSignIn() }
     }
 }
 
-private fun NavGraphBuilder.addPackagesScreen() {
+private fun NavGraphBuilder.addPackagesScreen(beginSignIn: suspend () -> IntentSenderRequest) {
     composable(route = Destination.PackagesMap.route) {
+        val context = LocalContext.current
+
+        val viewModel = hiltViewModel<PackagesMapViewModel>()
+        val uiState by viewModel.uiState.collectAsState(PackagesMapUiState())
+        val launcher = rememberLauncherForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            try {
+                val credentials = Identity
+                    .getSignInClient(context)
+                    .getSignInCredentialFromIntent(result.data)
+                viewModel.onAuthenticateWithGoogleSignIn(credentials.googleIdToken)
+            } catch (_: Exception) {
+            }
+        }
+
+        if (uiState.canBeginSignIn) {
+            LaunchedEffect(Unit) {
+                launcher.launch(beginSignIn())
+            }
+        }
+
         PackagesMapScreen()
     }
     composable(route = Destination.Package.route) {
