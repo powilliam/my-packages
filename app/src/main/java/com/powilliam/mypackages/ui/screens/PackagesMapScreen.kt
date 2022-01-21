43f12be4154b6f9802cf01149cfa685ac52aa86a
William Porto <william.porto@nave.rs>
2022-01-20 22:03:42 -0300

    feature(map): adding MapView interop with Jetpack Compose

--- app/src/main/java/com/powilliam/mypackages/ui/screens/PackagesMapScreen.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/screens/PackagesMapScreen.kt
@@ -1,20 +1,24 @@
 package com.powilliam.mypackages.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import com.powilliam.mypackages.ui.theming.MyPackagesTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.powilliam.mypackages.ui.composables.Map
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PackagesMapScreen() {
-}
-
-@Preview
-@Composable
-private fun PackagesMapScreenPreview() {
-    val context = LocalContext.current
-
-    MyPackagesTheme({ context }) {
-        PackagesMapScreen()
+fun PackagesMapScreen(modifier: Modifier = Modifier) {
+    Scaffold {
+        Box {
+            Map(
+                modifier
+                    .fillMaxSize()
+                    .align(Alignment.Center),
+            )
+        }
     }
 }
