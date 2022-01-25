4e4c6ba711320ab58c54ac6e5803e030ea683a97
William Porto <william.porto@nave.rs>
2022-01-25 11:29:24 -0300

    feature(graph): consuming ViewModel and displaying screen

--- app/src/main/java/com/powilliam/mypackages/ui/screens/SearchPackageScreen.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/screens/SearchPackageScreen.kt
@@ -1,20 +1,58 @@
 package com.powilliam.mypackages.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import com.powilliam.mypackages.ui.theming.MyPackagesTheme
+import com.powilliam.mypackages.ui.composables.AppBarTextField
+import com.powilliam.mypackages.ui.composables.Package
+import com.powilliam.mypackages.ui.viewmodels.SearchPackageUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchPackageScreen() {
-}
-
-@Preview
-@Composable
-private fun SearchPackageScreenPreview() {
-    val context = LocalContext.current
-
-    MyPackagesTheme({ context }) {
-        SearchPackageScreen()
+fun SearchPackageScreen(
+    uiState: SearchPackageUiState = SearchPackageUiState(),
+    onSearch: (String) -> Unit,
+    onNavigateToPreviousScreen: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            SearchPackageScreenAppBar(
+                value = uiState.query,
+                onValueChange = onSearch,
+                onNavigateToPreviousScreen = onNavigateToPreviousScreen
+            )
+        },
+    ) {
+        // TODO: Show all packages inside one LazyColumn
+        Column {
+            Package(tracker = { Text(text = "1234567676") }) {
+                Text(text = "Meu pacote")
+            }
+        }
     }
 }
+
+@Composable
+private fun SearchPackageScreenAppBar(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onNavigateToPreviousScreen: () -> Unit
+) {
+    Column {
+        AppBarTextField(
+            value = value,
+            placeholder = "Buscar por nome ou código de rastreio",
+            onValueChange = onValueChange,
+            navigationIcon = {
+                IconButton(onClick = onNavigateToPreviousScreen) {
+                    Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = null)
+                }
+            }
+        )
+        Divider(color = MaterialTheme.colorScheme.outline.copy(alpha = 0.12f))
+    }
+}
+
