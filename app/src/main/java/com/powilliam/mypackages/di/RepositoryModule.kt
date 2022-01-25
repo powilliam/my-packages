93428568c52d0919e1bba01cc28e0cd786e686ec
William Porto <william.porto@nave.rs>
2022-01-24 22:25:03 -0300

    feature(di): adding new data sources and repositories to their modules

--- app/src/main/java/com/powilliam/mypackages/di/RepositoryModule.kt
+++ app/src/main/java/com/powilliam/mypackages/di/RepositoryModule.kt
@@ -1,8 +1,11 @@
 package com.powilliam.mypackages.di
 
 import com.powilliam.mypackages.data.datasource.AuthRemoteDataSource
+import com.powilliam.mypackages.data.datasource.FeatureFlagRemoteDataSource
 import com.powilliam.mypackages.data.repository.AuthRepository
 import com.powilliam.mypackages.data.repository.AuthRepositoryImpl
+import com.powilliam.mypackages.data.repository.FeatureFlagRepository
+import com.powilliam.mypackages.data.repository.FeatureFlagRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +19,10 @@ object RepositoryModule {
     @Provides
     fun provideAuthRepository(authRemoteDataSource: AuthRemoteDataSource): AuthRepository =
         AuthRepositoryImpl(authRemoteDataSource)
+
+    @Singleton
+    @Provides
+    fun provideFeatureFlagRepository(
+        featureFlagRemoteDataSource: FeatureFlagRemoteDataSource
+    ): FeatureFlagRepository = FeatureFlagRepositoryImpl(featureFlagRemoteDataSource)
 }
