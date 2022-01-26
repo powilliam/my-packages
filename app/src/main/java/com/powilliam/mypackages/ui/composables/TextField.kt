ce5fc6c12ec307ebc536c92e8805df2dedd9260c
William Porto <william.porto@nave.rs>
2022-01-26 20:29:01 -0300

    feature(composables): adding FormTextField

--- app/src/main/java/com/powilliam/mypackages/ui/composables/TextField.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/composables/TextField.kt
@@ -3,6 +3,7 @@ package com.powilliam.mypackages.ui.composables
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.Divider
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -69,4 +70,71 @@ fun AppBarTextField(
             }
         )
     }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun FormTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    placeholder: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    icon: (@Composable () -> Unit)? = null
+) {
+    val focus = LocalFocusManager.current
+    val softwareKeyboardController = LocalSoftwareKeyboardController.current
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        textStyle = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface),
+        keyboardActions = KeyboardActions(
+            onAny = {
+                focus.clearFocus()
+                softwareKeyboardController?.hide()
+            }
+        ),
+        decorationBox = { innerTextField ->
+            Surface(modifier.fillMaxWidth()) {
+                Column {
+                    Row(
+                        modifier.padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Box(modifier.size(24.dp)) {
+                            icon?.let { composable ->
+                                Box(modifier.align(Alignment.Center)) {
+                                    composable()
+                                }
+                            }
+                        }
+                        Column(
+                            modifier.padding(end = 16.dp),
+                            verticalArrangement = Arrangement.spacedBy(2.dp)
+                        ) {
+                            Text(
+                                text = label,
+                                style = MaterialTheme.typography.labelSmall.copy(color = MaterialTheme.colorScheme.outline)
+                            )
+                            Box {
+                                innerTextField()
+                                if (value.isEmpty()) {
+                                    Text(
+                                        modifier = modifier.align(Alignment.CenterStart),
+                                        text = placeholder,
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Divider(color = MaterialTheme.colorScheme.outline.copy(alpha = 0.12f))
+                }
+            }
+        }
+    )
 }
