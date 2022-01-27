a87f4761250b0339004da5515d21d73c4cb28a55
William Porto <william.porto@nave.rs>
2022-01-27 20:46:32 -0300

    fix(auth-repository): getAuthenticatedAccount emit its value each second

--- app/src/main/java/com/powilliam/mypackages/data/repository/AuthRepository.kt
+++ app/src/main/java/com/powilliam/mypackages/data/repository/AuthRepository.kt
@@ -4,8 +4,8 @@ import com.google.firebase.auth.AuthResult
 import com.powilliam.mypackages.data.datasource.AuthProvider
 import com.powilliam.mypackages.data.datasource.AuthRemoteDataSource
 import com.powilliam.mypackages.data.entity.UserEntity
+import com.powilliam.mypackages.utils.withInterval
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 interface AuthRepository {
@@ -17,16 +17,19 @@ interface AuthRepository {
 class AuthRepositoryImpl @Inject constructor(
     private val authRemoteDataSource: AuthRemoteDataSource
 ) : AuthRepository {
-    override suspend fun getAuthenticatedAccount(): Flow<UserEntity?> = flow {
-        val account = authRemoteDataSource.getAuthenticatedAccount()
-        account?.let {
-            emit(UserEntity(id = it.uid, email = it.email, avatar = it.photoUrl))
+    override suspend fun getAuthenticatedAccount(): Flow<UserEntity?> =
+        withInterval(ONE_SECOND_IN_MILLISECONDS) {
+            val account = authRemoteDataSource.getAuthenticatedAccount() ?: return@withInterval null
+            UserEntity(id = account.uid, email = account.email, avatar = account.photoUrl)
         }
-    }
 
     override suspend fun authenticateWithGoogleSignIn(idToken: String?) =
         authRemoteDataSource.authenticate(AuthProvider.GoogleProvider(idToken))
 
     override suspend fun unAuthenticate() =
         authRemoteDataSource.unAuthenticate()
+
+    companion object {
+        private const val ONE_SECOND_IN_MILLISECONDS = 1000L
+    }
 }
