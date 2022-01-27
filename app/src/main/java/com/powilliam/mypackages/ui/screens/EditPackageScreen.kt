7e083dec659582ec89949eb53ff2541b60c3b319
William Porto <william.porto@nave.rs>
2022-01-26 23:02:38 -0300

    feature(screens): adding EditPackageScreen with its viewmodel

--- app/src/main/java/com/powilliam/mypackages/ui/screens/EditPackageScreen.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/screens/EditPackageScreen.kt
@@ -1,20 +1,65 @@
 package com.powilliam.mypackages.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import com.powilliam.mypackages.ui.theming.MyPackagesTheme
+import androidx.compose.ui.Modifier
+import com.google.accompanist.insets.statusBarsPadding
+import com.powilliam.mypackages.ui.composables.FormTextField
+import com.powilliam.mypackages.ui.viewmodels.EditPackageUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditPackageScreen() {
+fun EditPackageScreen(
+    uiState: EditPackageUiState = EditPackageUiState(),
+    onNavigateToPreviousScreen: () -> Unit,
+    onSubmit: () -> Unit,
+    onChangePackageName: (String) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            EditPackageScreenAppBar(
+                canSubmit = uiState.canSubmit,
+                onNavigateToPreviousScreen = onNavigateToPreviousScreen,
+                onSubmit = onSubmit
+            ) {
+                Text(text = "12412412125124")
+            }
+        }
+    ) {
+        Column {
+            FormTextField(
+                value = uiState.packageName,
+                placeholder = "Adicione um nome para o pacote",
+                label = "Nome do pacote",
+                onValueChange = onChangePackageName
+            )
+        }
+    }
 }
 
-@Preview
 @Composable
-private fun EditPackageScreenPreview() {
-    val context = LocalContext.current
-
-    MyPackagesTheme({ context }) {
-        EditPackageScreen()
-    }
+private fun EditPackageScreenAppBar(
+    modifier: Modifier = Modifier,
+    canSubmit: Boolean = false,
+    onNavigateToPreviousScreen: () -> Unit,
+    onSubmit: () -> Unit,
+    tracker: @Composable () -> Unit,
+) {
+    LargeTopAppBar(
+        modifier = modifier.statusBarsPadding(),
+        navigationIcon = {
+            IconButton(onClick = onNavigateToPreviousScreen) {
+                Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = null)
+            }
+        },
+        title = { tracker() },
+        actions = {
+            Button(onClick = { if (canSubmit) onSubmit() }, enabled = canSubmit) {
+                Text(text = "Feito")
+            }
+        }
+    )
 }
