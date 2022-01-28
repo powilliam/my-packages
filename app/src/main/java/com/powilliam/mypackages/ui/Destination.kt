22763ef162b47f7c9036d1eabceb1afed7b076f3
William Porto <william.porto@nave.rs>
2022-01-28 01:50:00 -0300

    feature(package): displaying single package with realtime support

--- app/src/main/java/com/powilliam/mypackages/ui/Destination.kt
+++ app/src/main/java/com/powilliam/mypackages/ui/Destination.kt
@@ -2,7 +2,7 @@ package com.powilliam.mypackages.ui
 
 sealed class Destination(val route: String) {
     object PackagesMap : Destination("/packages")
-    object Package : Destination("/packages?packageId={packageId}")
+    object Package : Destination("/packages?tracker={tracker}")
     object AddPackage : Destination("/packages/create")
     object EditPackage : Destination("/packages/edit?packageId={packageId}")
     object SearchPackage : Destination("/packages/search")
