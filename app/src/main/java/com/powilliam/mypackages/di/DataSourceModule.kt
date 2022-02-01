204974be096b7715d5e79a8756be195433b10c0e
William Porto <william.porto@nave.rs>
2022-02-01 15:21:33 -0300

    feature(messaging): adding and setup firebase messaging

--- app/src/main/java/com/powilliam/mypackages/di/DataSourceModule.kt
+++ app/src/main/java/com/powilliam/mypackages/di/DataSourceModule.kt
@@ -3,6 +3,7 @@ package com.powilliam.mypackages.di
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.powilliam.mypackages.data.datasource.*
 import dagger.Module
@@ -28,4 +29,14 @@ object DataSourceModule {
     @Provides
     fun providePackageRemoteDataSource(): PackageRemoteDataSource =
         PackageRemoteDataSourceImpl(Firebase.database.reference)
+
+    @Singleton
+    @Provides
+    fun provideDeviceTokenLocalDataSource(): DeviceTokenLocalDataSource =
+        DeviceTokenLocalDataSourceImpl(Firebase.messaging)
+
+    @Singleton
+    @Provides
+    fun provideUserSettingsRemoteDataSource(): UserSettingsRemoteDataSource =
+        UserSettingsRemoteDataSourceImpl(Firebase.database.reference)
 }
