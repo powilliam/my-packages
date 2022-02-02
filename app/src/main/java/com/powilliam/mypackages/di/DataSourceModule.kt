f657c849237b753e0484554812baf7b167db47b4
William Porto <william.porto@nave.rs>
2022-02-02 15:30:56 -0300

    feature(notifications): adding datasources for notifications

--- app/src/main/java/com/powilliam/mypackages/di/DataSourceModule.kt
+++ app/src/main/java/com/powilliam/mypackages/di/DataSourceModule.kt
@@ -1,14 +1,18 @@
 package com.powilliam.mypackages.di
 
+import android.content.Context
+import androidx.work.WorkManager
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.powilliam.mypackages.data.database.NotificationAccessObject
 import com.powilliam.mypackages.data.datasource.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -39,4 +43,15 @@ object DataSourceModule {
     @Provides
     fun provideUserSettingsRemoteDataSource(): UserSettingsRemoteDataSource =
         UserSettingsRemoteDataSourceImpl(Firebase.database.reference)
+
+    @Singleton
+    @Provides
+    fun provideNotificationLocalDataSource(
+        notificationAccessObject: NotificationAccessObject
+    ): NotificationLocalDataSource = NotificationLocalDataSourceImpl(notificationAccessObject)
+
+    @Singleton
+    @Provides
+    fun provideNotificationTaskDataSource(@ApplicationContext context: Context): NotificationTaskDataSource =
+        NotificationTaskDataSourceImpl(WorkManager.getInstance(context))
 }
